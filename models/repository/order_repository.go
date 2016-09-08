103316e2c5910378959c88d330e1ad2f00f79413
Tran Tam <tranminhtam.10192@gmail.com>
2016-09-08 15:01:05 +0700

    cont...

--- models/repository/order_repository.go
+++ models/repository/order_repository.go
@@ -10,18 +10,18 @@ import (
 	"time"
 )
 
-type CartRepository struct {
+type OrderRepository struct {
 	*DB
 }
 
-func (repo CartRepository) generateAccessToken(cart *Cart) error {
+func (repo OrderRepository) generateAccessToken(order *Order) error {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 try_gen_other_value:
-	cart.AccessToken = fake.CharactersN(32)
+	order.AccessToken = fake.CharactersN(32)
 
-	temp_cart := &Cart{}
-	if err := repo.DB.Where("access_token = ?", cart.AccessToken).Find(temp_cart).Error; err != nil {
+	temp_order := &Order{}
+	if err := repo.DB.Where("access_token = ?", order.AccessToken).Find(temp_order).Error; err != nil {
 		// Check if err is not found -> access_token is unique
 		if err.Error() == "record not found" {
 			return nil
@@ -35,40 +35,40 @@ try_gen_other_value:
 	goto try_gen_other_value
 }
 
-func (repo CartRepository) getCartItemsInfo(cart *Cart) {
-	for idx, item := range cart.Items {
+func (repo OrderRepository) getOrderItemsInfo(order *Order) {
+	for idx, item := range order.Items {
 		product := &Product{}
 		product.Id = item.ProductId
 		repo.DB.Select("name, price, image, image_updated_at").Find(&product)
-		cart.Items[idx].ProductPrice = product.Price
-		cart.Items[idx].ProductName = product.Name
+		order.Items[idx].ProductPrice = product.Price
+		order.Items[idx].ProductName = product.Name
 		(*product).GetImageUrl()
-		cart.Items[idx].ProductImageThumbnailUrl = product.ImageThumbnailUrl
+		order.Items[idx].ProductImageThumbnailUrl = product.ImageThumbnailUrl
 	}
 }
 
-func (repo CartRepository) clearNullQuantity() {
-	repo.DB.Delete(CartItem{}, "quantity=0")
+func (repo OrderRepository) clearNullQuantity() {
+	repo.DB.Delete(OrderItem{}, "quantity=0")
 }
 
-func (repo CartRepository) createCart(cart *Cart) error {
-	if err := repo.generateAccessToken(cart); err != nil {
+func (repo OrderRepository) createOrder(order *Order) error {
+	if err := repo.generateAccessToken(order); err != nil {
 		return err
 	}
 
-	if err := repo.DB.Create(cart).Error; err != nil {
+	if err := repo.DB.Create(order).Error; err != nil {
 		return err
 	}
 
-	repo.getCartItemsInfo(cart)
+	repo.getOrderItemsInfo(order)
 
 	return nil
 }
 
-func (repo CartRepository) updateCart(cart *Cart) error {
+func (repo OrderRepository) updateOrder(order *Order) error {
 	tx := repo.DB.Begin()
 
-	if err := tx.Save(cart).Error; err != nil {
+	if err := tx.Save(order).Error; err != nil {
 		tx.Rollback()
 		return err
 	}
@@ -78,48 +78,48 @@ func (repo CartRepository) updateCart(cart *Cart) error {
 	repo.clearNullQuantity()
 
 	// Don't return access_token when updating
-	cart.EraseAccessToken()
+	order.EraseAccessToken()
 
-	repo.getCartItemsInfo(cart)
+	repo.getOrderItemsInfo(order)
 
 	return nil
 }
 
-func (repo CartRepository) GetCart(access_token string) (*Cart, error) {
-	cart := &Cart{}
-	items := &[]CartItem{}
+func (repo OrderRepository) GetOrder(access_token string) (*Order, error) {
+	order := &Order{}
+	items := &[]OrderItem{}
 
-	// find a cart by its access_token
-	if err := repo.DB.Where("access_token = ?", access_token).Find(cart).Error; err != nil {
+	// find a order by its access_token
+	if err := repo.DB.Where("access_token = ?", access_token).Find(order).Error; err != nil {
 		return nil, err
 	}
 
-	// use cart.Id to find its CartItem data (cart.Id is its forein key)
-	if err := repo.DB.Where("cart_id = ?", cart.Id).Find(items).Error; err != nil {
+	// use order.Id to find its OrderItem data (order.Id is its forein key)
+	if err := repo.DB.Where("order_id = ?", order.Id).Find(items).Error; err != nil {
 		return nil, err
 	}
 
-	// use the cart.Items to update products information
-	cart.Items = *items
-	repo.getCartItemsInfo(cart)
+	// use the order.Items to update products information
+	order.Items = *items
+	repo.getOrderItemsInfo(order)
 
-	return cart, nil
+	return order, nil
 }
 
-func (repo CartRepository) SaveCart(cart *Cart) error {
-	if cart.AccessToken == "" {
-		return repo.createCart(cart)
+func (repo OrderRepository) SaveOrder(order *Order) error {
+	if order.AccessToken == "" {
+		return repo.createOrder(order)
 	}
-	return repo.updateCart(cart)
+	return repo.updateOrder(order)
 }
 
-func (repo CartRepository) DeleteCartItem(cart *Cart, item_id uint) error {
-	item := CartItem{}
+func (repo OrderRepository) DeleteOrderItem(order *Order, item_id uint) error {
+	item := OrderItem{}
 
-	// use cart.Id to find its CartItem data (cart.Id is its forein key)
-	if err := repo.DB.Where("id = ? AND cart_id = ?", item_id, cart.Id).Find(&item).Error; err != nil {
+	// use order.Id to find its OrderItem data (order.Id is its forein key)
+	if err := repo.DB.Where("id = ? AND order_id = ?", item_id, order.Id).Find(&item).Error; err != nil {
 		if err.Error() == "record not found" {
-			return errors.New("Not found Item Id in Cart")
+			return errors.New("Not found Item Id in Order")
 		}
 
 		return err
@@ -127,12 +127,12 @@ func (repo CartRepository) DeleteCartItem(cart *Cart, item_id uint) error {
 
 	repo.DB.Delete(&item)
 
-	// use cart.Id to find its CartItem data (cart.Id is its forein key)
-	items := &[]CartItem{}
-	repo.DB.Where("cart_id = ?", cart.Id).Find(items)
-	cart.Items = *items
+	// use order.Id to find its OrderItem data (order.Id is its forein key)
+	items := &[]OrderItem{}
+	repo.DB.Where("order_id = ?", order.Id).Find(items)
+	order.Items = *items
 
-	repo.getCartItemsInfo(cart)
+	repo.getOrderItemsInfo(order)
 
 	return nil
 }
