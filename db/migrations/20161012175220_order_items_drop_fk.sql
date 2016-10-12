00180f7fae85ce0aea1553557cb1df24bc5e28e5
Tran Tam <tranminhtam.10192@gmail.com>
2016-10-13 01:21:48 +0700

    update db schema + fix product_status enum type + remove order_items foreign key constrain

---
+++ db/migrations/20161012175220_order_items_drop_fk.sql
@@ -0,0 +1,8 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE order_items DROP CONSTRAINT "order_items_product_id_fkey";
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+ALTER TABLE order_items ADD FOREIGN KEY(product_id) REFERENCES products(id);
